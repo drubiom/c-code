9584b00abd2d46bbbae8c1d776d9f9f4b49f15fd
David Rubio <davidrubio.95@gmail.com>
2016-11-14 10:36:02 +0100

    devuelve si un número introducido es perfecto

--- 01_varios/perfectos.cpp
+++ 01_varios/perfectos.cpp
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(){
-    int n,x,contador;
+    long long int n,x,contador;
 
-    do{
-    contador=0;
-    printf("Introduce un número: (o cero para salir)\n");
-    scanf("%i",&n);
-        for(x=1;x<n;x++){
-          if (n%x==0){
-              contador+=x;
-          }
-        }
-      if (contador==n){
-          printf("ES PERFECTO\n");
-      }
-     else{
-         printf("NO ES PERFECTO\n");
-     }
-    }while(n!=0);
+    do {
+        contador = 0;
+        printf("Introduce un número: (o cero para salir)\n");
+        scanf("%lli",&n);
+
+        for(x=1; x<n; x++)
+            if (n % x == 0)
+                contador+=x;
+
+        if (contador == n)
+            printf("ES PERFECTO\n");
+        else
+            printf("NO ES PERFECTO\n");
+
+    } while (n!=0);
+
     return EXIT_SUCCESS;
 }
