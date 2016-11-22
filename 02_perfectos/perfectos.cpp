e108f255db5b6fe000ffa2b133703a1657f1244e
David Rubio <davidrubio.95@gmail.com>
2016-11-22 10:21:07 +0100

    Devuelve si un número es perfecto

---
+++ 02_perfectos/perfectos.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    long long int n,x,contador;
+
+    do {
+        contador = 0;
+        printf("Introduce un número: (o cero para salir)\n");
+        scanf("%lli",&n);
+
+        for(x=1; x<n; x++)
+            if (n % x == 0)
+                contador+=x;
+
+        if (contador == n)
+            printf("ES PERFECTO\n");
+        else
+            printf("NO ES PERFECTO\n");
+
+    } while (n!=0);
+
+    return EXIT_SUCCESS;
+}
