938a05ef0475bedca85477c93edd6b9cab0214bf
David Rubio <davidrubio.95@gmail.com>
2017-01-17 11:04:26 +0100

    Ejercicios Navidad

--- Ejercicios_Navidad/02-Tipos_de_datos/01-Caracteres/01.cpp
+++ Ejercicios_Navidad/02-Tipos_de_datos/01-Caracteres/01.cpp
@@ -2,14 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX 50
+
 //Calcula la distancia entre dos letras introducidas por el usuario. Usa fgetc.
 
 int main(){
     char car1, car2;
-    printf("Introduce la primera letra: ");
-    scanf(" %c", &car1);
-    printf("Introduce la segunda letra: ");
-    scanf(" %c", &car2);
+    printf("Introduce dos letras seguidas: ");
+    car1 = fgetc(stdin);
+    car2 = fgetc(stdin);
     if(car1 > car2)
         printf("La distancia entre letras es: %i\n", car1 - car2);
     else if (car1 < car2)
