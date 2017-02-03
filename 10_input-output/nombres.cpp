2820ad5a2c9528f7b5e92f5c304777d162661792
David Rubio <davidrubio.95@gmail.com>
2017-02-03 13:30:19 +0100

    nombres

---
+++ 10_input-output/nombres.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 100
+
+int main(int argc, char *argv[]){
+    char buffer[N];
+    char *palabra;
+    printf("Nombre:");
+    fgets(buffer,N,stdin);
+    palabra = (char *) malloc(strlen(buffer) + 1);
+    strcpy(palabra, buffer);
+    printf("%s", palabra);
+    free(palabra);
+    return EXIT_SUCCESS;
+}
