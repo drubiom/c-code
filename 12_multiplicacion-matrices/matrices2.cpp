1e803894901e6e5dcd1c1aea80a7d0b3376babbe
David Rubio <davidrubio.95@gmail.com>
2017-03-09 11:21:10 +0100

    Update matrices2.cpp

--- 12_multiplicacion-matrices/matrices2.cpp
+++ 12_multiplicacion-matrices/matrices2.cpp
@@ -8,15 +8,12 @@ int main(){
    int A[N][N] = { {3,2,6}, {5,9,1}, {3,4,4} }; 
    int B[N][N] = { {6,3,9}, {1,2,1}, {2,2,8} };
    int C[N][N];
-   int sum;
    
    bzero(C, sizeof(C));
    for (int i = 0; i < N ; i++){
    	for (int j = 0; j < N; j++){
-   		sum=0;
    		for (int k = 0; k < N; k++){
-   				sum += A[i][k] * B[k][j];
-   				C[i][j] = sum;
+   				C[i][j] += A[i][k] * B[k][j];
          }
    		printf("%i ", C[i][j]);
    	}
