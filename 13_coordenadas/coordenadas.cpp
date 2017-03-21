b6a33b22b4446544f5c17b53dd7d9e2ad181d341
David Rubio <davidrubio.95@gmail.com>
2017-03-21 13:48:18 +0100

    Transforma coordenadas a pantalla

---
+++ 13_coordenadas/coordenadas.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define N 1
+
+struct TCoordenadas
+{
+	int x;
+	int y;
+	
+};
+
+
+TCoordenadas cambiarcoordenadas( struct TCoordenadas);
+
+int main(int argc, char const *argv[])
+{
+	TCoordenadas coord;
+	printf("Introduce fila\n");
+	scanf(" %i", &coord.x);
+	printf("Introduce columna\n");
+	scanf(" %i", &coord.y);
+	printf("La fila en la que se imprime es: %i\nLa columna en la que se imprime es: %i\n", cambiarcoordenadas(coord).x, cambiarcoordenadas(coord).y);
+
+	return 0;
+}
+
+TCoordenadas cambiarcoordenadas(TCoordenadas coord){
+	TCoordenadas temp;
+	if (coord.x == 0)
+		temp.y = 0;
+	if (coord.y == 0)
+		temp.x = 0;
+	else{
+		temp.x = coord.y + N;
+		temp.y = coord.x + N;
+	}
+	return temp;
+}
